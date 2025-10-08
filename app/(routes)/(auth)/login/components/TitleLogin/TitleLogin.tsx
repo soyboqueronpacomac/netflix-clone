f637b30a7b9fe17f423ddb944e1fd00a2504b77a
Your Name <you@example.com>
2025-10-08 12:25:30 +0200

    Rutas iniciales

---
+++ app/(routes)/(auth)/login/components/TitleLogin/TitleLogin.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export  function TitleLogin({children}: {children: React.ReactNode}) {
+  return (
+    <h1 className="text-2xl font-bold">{children}</h1>
+  )
+}
---
+++ app/(routes)/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TitleLogin } from '@/app/(routes)/(auth)/login/components'
+
+
+export default function PageLogin() {
+  return (
+    <>
+      <TitleLogin>Log in</TitleLogin>
+    </>
+  )
+}
