c398b4ce77245b7d85593fb716351b719379c00d
Your Name <you@example.com>
2025-10-08 16:23:34 +0200

    efecto navbar fondo

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function PageHome() {
   return (
     <div className="relative bg-zinc-900">
       <Navbar />
+      
     </div>
   );
 }
--- components/Shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
+++ components/Shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,13 +1,21 @@
+"use client"
+
 import { cn } from '@/lib'
 import { BellRing, Search } from 'lucide-react'
 
 import React from 'react'
 import { Logo, ItemsNavbar, ItemsAuth } from "@/components/Shared"
+import { useScrollPosition } from '@/hooks';
 
 
 export  function NavbarDesktop() {
+    const scrollPosition = useScrollPosition();
+    console.log(scrollPosition);
   return (
-    <div className={cn("z-30 fixed top-0 left-0 right-0 transition-all duration-300")}>
+    <div className={cn(
+        "z-30 fixed top-0 left-0 right-0 transition-all duration-300",
+        scrollPosition > 20 ? "bg-black p-4" : "bg-transparent"
+        )}>
         <div className="px-[4%] mx-auto h-full">
             <div className="flex gap-4 justify-between items-center h-full">
                 <div className='flex gap-2 items-center'>
